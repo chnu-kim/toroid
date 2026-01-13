26f877f454fc37985968cde4db77037c95d9504f
chnu-kim <chanuuuu@naver.com>
2026-01-14 02:40:00 +0900

    Initialize Spring Boot application structure

---
+++ src/test/kotlin/me/chnu/chzzk/ChzzkApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.chnu.chzzk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChzzkApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
