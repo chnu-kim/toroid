8e915aedaf49f580ad9dbc6b5f7e13b15e52f843
chnu-kim <chanuuuu@naver.com>
2026-01-26 01:56:13 +0900

    Implement in-memory token storage with `storeToken` and `getToken`

--- src/main/kotlin/me/chnu/toroid/infrastructure/chzzk/auth/MemoryChzzkTokenStorage.kt
+++ src/main/kotlin/me/chnu/toroid/infrastructure/chzzk/auth/MemoryChzzkTokenStorage.kt
@@ -2,18 +2,19 @@ package me.chnu.toroid.infrastructure.chzzk.auth
 
 import me.chnu.toroid.domain.chzzk.auth.ChzzkTokenStorage
 import org.springframework.stereotype.Component
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.time.Duration
 
 @Component
 class MemoryChzzkTokenStorage : ChzzkTokenStorage {
+    private val repository: ConcurrentHashMap<String, String> = ConcurrentHashMap()
 
     override fun storeToken(key: String, value: String, ttl: Duration) {
-        TODO("Not yet implemented")
+        repository[key] = value
     }
 
     override fun getToken(key: String): String? {
-        TODO("Not yet implemented")
+        return repository[key]
     }
-
 
 }
