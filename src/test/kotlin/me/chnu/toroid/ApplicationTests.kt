a2bcd53152f5b245c5ddee6a0ceeecff13db05c5
chnu-kim <chanuuuu@naver.com>
2026-02-20 14:40:04 +0900

    Remove unused Spring test dependencies from ApplicationTests

--- src/test/kotlin/me/chnu/toroid/ApplicationTests.kt
+++ src/test/kotlin/me/chnu/toroid/ApplicationTests.kt
@@ -1,13 +1,3 @@
 package me.chnu.toroid
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class ApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
-}
+class ApplicationTests
