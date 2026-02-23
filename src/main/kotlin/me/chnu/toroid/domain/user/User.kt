c1373fc4a73815d5b215b19eeea3399d12f70aba
chnu-kim <chanuuuu@naver.com>
2026-02-23 09:31:35 +0900

    Set `auth` schema for User ID sequence generator

--- src/main/kotlin/me/chnu/toroid/domain/user/User.kt
+++ src/main/kotlin/me/chnu/toroid/domain/user/User.kt
@@ -14,6 +14,7 @@ import java.time.OffsetDateTime
 private const val USER_ID_SEQ_GENERATOR = "USER_ID_SEQ_GENERATOR"
 
 @SequenceGenerator(
+    schema = "auth",
     name = USER_ID_SEQ_GENERATOR,
     sequenceName = "user_id_seq",
     initialValue = 1,
