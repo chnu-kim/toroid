18fe87d2c20ff870052e3122419dc0cd3a51908e
chnu-kim <chanuuuu@naver.com>
2026-03-06 14:04:56 +0900

    Add version catalog to manage dependencies with `libs.versions.toml`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import dev.detekt.gradle.Detekt
 
 plugins {
-    kotlin("jvm") version "2.2.20"
-    kotlin("plugin.spring") version "2.2.20"
-    kotlin("plugin.jpa") version "2.2.20"
-    id("org.springframework.boot") version "4.0.1"
-    id("io.spring.dependency-management") version "1.1.7"
-    id("dev.detekt") version ("2.0.0-alpha.1")
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.kotlin.jpa)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.detekt)
 }
 
 group = "me.chnu"
@@ -24,30 +24,30 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-webmvc")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-opentelemetry")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("tools.jackson.module:jackson-module-kotlin")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.apache.httpcomponents.client5:httpclient5")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("com.auth0:java-jwt:4.5.0")
-    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
-    implementation("io.socket:socket.io-client:1.0.2")
-    testImplementation("org.springframework.boot:spring-boot-starter-security-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("io.kotest:kotest-assertions-core:6.0.2")
-    testImplementation("io.kotest:kotest-runner-junit5:6.0.2")
-    testImplementation("com.lemonappdev:konsist:0.17.3")
-    testImplementation("io.mockk:mockk:1.14.2")
-    runtimeOnly("org.postgresql:postgresql")
-    runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(libs.spring.boot.starter.security)
+    implementation(libs.spring.boot.starter.webmvc)
+    implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.spring.boot.starter.opentelemetry)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.jackson.module.kotlin)
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.httpclient5)
+    implementation(libs.spring.boot.starter.data.redis)
+    implementation(libs.java.jwt)
+    implementation(libs.kotlin.logging)
+    implementation(libs.socket.io.client)
+    testImplementation(libs.spring.boot.starter.security.test)
+    testImplementation(libs.spring.boot.starter.webmvc.test)
+    testImplementation(libs.kotlin.test.junit5)
+    testImplementation(libs.kotest.assertions.core)
+    testImplementation(libs.kotest.runner.junit5)
+    testImplementation(libs.konsist)
+    testImplementation(libs.mockk)
+    runtimeOnly(libs.postgresql)
+    runtimeOnly(libs.spring.boot.docker.compose)
+    testRuntimeOnly(libs.junit.platform.launcher)
 
-    detektPlugins("dev.detekt:detekt-rules-ktlint-wrapper:2.0.0-alpha.1")
+    detektPlugins(libs.detekt.rules.ktlint.wrapper)
 }
 
 kotlin {
