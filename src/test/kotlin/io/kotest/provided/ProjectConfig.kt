e68a9c2f561d23fbc77b0210f91ef8581e9a55c8
chnu-kim <chanuuuu@naver.com>
2026-03-06 12:28:06 +0900

    Set global `specExecutionMode` in `ProjectConfig` and update tests

--- src/test/kotlin/io/kotest/provided/ProjectConfig.kt
+++ src/test/kotlin/io/kotest/provided/ProjectConfig.kt
@@ -2,7 +2,7 @@ package io.kotest.provided
 
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.spec.IsolationMode
-import io.kotest.core.spec.SpecExecutionMode
+import io.kotest.engine.concurrency.SpecExecutionMode
 
 object ProjectConfig : AbstractProjectConfig() {
     override val isolationMode = IsolationMode.InstancePerRoot
--- src/test/kotlin/io/kotest/provided/ProjectConfigTest.kt
+++ src/test/kotlin/io/kotest/provided/ProjectConfigTest.kt
@@ -2,11 +2,16 @@ package io.kotest.provided
 
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.engine.concurrency.SpecExecutionMode
 import io.kotest.matchers.shouldBe
 
 class ProjectConfigTest : StringSpec({
 
     "isolationMode가 InstancePerRoot로 설정되어 있다" {
         ProjectConfig.isolationMode shouldBe IsolationMode.InstancePerRoot
+    }
+
+    "specExecutionMode가 Concurrent로 설정되어 있다" {
+        ProjectConfig.specExecutionMode shouldBe SpecExecutionMode.Concurrent
     }
 })
